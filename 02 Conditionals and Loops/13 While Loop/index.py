19bae84a462b904a67f6345d2c009f0920b2f459
abuzar-ahmad <134150996+abuzar-ahmad@users.noreply.github.com>
2023-12-14 23:19:22 +0530

    Create index.py

---
+++ 02 Conditionals and Loops/13 While Loop/index.py
@@ -0,0 +1,13 @@
+n = 10
+i = 0
+while i <= n:
+    print(i)
+    i = i + 1
+
+
+##############################################
+n = int(input())
+count = 1
+while count <= n:
+    print(count)
+    count = count + 1
