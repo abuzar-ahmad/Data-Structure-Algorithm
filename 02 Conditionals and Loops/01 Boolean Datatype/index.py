82a58bde4d14942321f52669056be38a597ec60e
abuzar-ahmad <134150996+abuzar-ahmad@users.noreply.github.com>
2023-12-14 23:07:24 +0530

    Create index.py

---
+++ 02 Conditionals and Loops/01 Boolean Datatype/index.py
@@ -0,0 +1,29 @@
+a = True
+b = False
+
+c = 'Ninjas'
+print(c)
+print(a)
+print(b)
+print(type(a))
+
+
+# Relational Operators
+a = 10
+b = 20
+print(a > b)
+print(a < b)
+print(a <= b)
+print(a >= b)
+print(a == b)
+print(a != b)
+
+# Logical Operators
+c1 = a > 10
+c2 = b > 10
+
+print(c1 and c2)
+print(c1 or c2)
+
+
+
