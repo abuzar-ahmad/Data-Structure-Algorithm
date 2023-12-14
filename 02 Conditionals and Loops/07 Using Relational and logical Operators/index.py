46ced9928b68a0e3545fc8dde993ed0325db94aa
abuzar-ahmad <134150996+abuzar-ahmad@users.noreply.github.com>
2023-12-14 23:13:56 +0530

    Create index.py

---
+++ 02 Conditionals and Loops/07 Using Relational and logical Operators/index.py
@@ -0,0 +1,19 @@
+a = int(input())
+b = int(input())
+
+ 
+
+
+if a > 10 and b > 10:
+    print('Both > 10')
+else:
+    print('No they are not')
+
+
+##########################
+
+a = int(input())
+if a == 7:
+    print('it is 7')
+else:
+    print('no its not 7')
