e018322f36a6a952cfe994319b652bd1f6bd7431
abuzar-ahmad <134150996+abuzar-ahmad@users.noreply.github.com>
2023-12-14 22:46:14 +0530

    Create index.py

---
+++ 01 Introduction to Paytho/10 Python Numbers/index.py
@@ -0,0 +1,18 @@
+a1 = 23
+a2 = 3.4
+a3 = 4 + 5j
+print(type(a1))
+print(type(a2))
+print(type(a3))
+
+a4 = 10
+print(id(a4))
+a5 = a4 + 1
+print(id(a5))
+
+
+a6 = 10
+print(id(a6))
+a7 = 10
+print(id(a7))
+# Id of numbers will be same for numbers -5 to 256
