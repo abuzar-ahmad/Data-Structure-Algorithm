b6ec6b1fd261dba7f78e58e77aaaf15d5bca6330
abuzar-ahmad <134150996+abuzar-ahmad@users.noreply.github.com>
2023-12-14 23:08:54 +0530

    Create index.py

---
+++ 02 Conditionals and Loops/02 Introduction to If Else/index.py
@@ -0,0 +1,17 @@
+a = True
+if a:
+    print('I am inside if')
+    print('I am inside if2')
+else:
+    print('I am in else')
+
+
+
+
+# check number to be or even
+n = int(input())
+# remainder = n % 2
+if(n % 2 == 0):
+    print('even')
+else:
+    print('odd')
