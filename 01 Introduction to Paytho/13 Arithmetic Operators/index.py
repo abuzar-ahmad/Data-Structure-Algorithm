b49ac32c7747dcd94915296803ae018122e514fc
abuzar-ahmad <134150996+abuzar-ahmad@users.noreply.github.com>
2023-12-14 22:50:58 +0530

    Create index.py

---
+++ 01 Introduction to Paytho/13 Arithmetic Operators/index.py
@@ -0,0 +1,23 @@
+a = 10
+b = 4
+print(a - b)
+print(a + b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a ** b)
+print(a % b)
+print(2 + 3 * 4)
+print(2 * 3 // 4)
+
+# Simple Interest
+p = 100
+r = 12
+t = 2
+si = (p * r * t ) // 100
+print(si)
+
+#  far to celcius conversion
+f = 100
+c = (f - 32) * 5 / 9
+print(c)
