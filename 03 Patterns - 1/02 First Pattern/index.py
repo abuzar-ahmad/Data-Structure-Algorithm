2ced43450184e12422b9da3164465a85a313b5c7
abuzar-ahmad <134150996+abuzar-ahmad@users.noreply.github.com>
2023-12-14 23:35:10 +0530

    Create index.py

---
+++ 03 Patterns - 1/02 First Pattern/index.py
@@ -0,0 +1,17 @@
+"""
+Create the given pattern
+    * * * *
+    * * * *
+    * * * *
+    * * * *
+"""
+
+n = int(input())
+i = 1
+while i <= n:
+    j = 1
+    while j <= n:
+        print('*', end=' ')
+        j = j + 1
+    print()
+    i = i + 1
