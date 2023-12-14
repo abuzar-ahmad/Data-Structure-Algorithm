1c8732bdc44f544447784d85efca2e7b46bc287c
abuzar-ahmad <134150996+abuzar-ahmad@users.noreply.github.com>
2023-12-14 23:14:54 +0530

    Create index.py

---
+++ 02 Conditionals and Loops/08 Using Else If/index.py
@@ -0,0 +1,22 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a >= b and a >= c:
+    print(a)
+elif b >= a and b >= c:
+    print(b)
+else:
+    print(c)
+
+
+#############################################
+n = int(input())
+if n > 10:
+    print('red')
+elif n >= 5 and n <= 10:
+    print('green')
+elif n > 0 and n < 5:
+    print('yello')
+    
+    
